b8f0106f77551385deaea58459b82322478afc41
Algaba <89382004+Algaba84@users.noreply.github.com>
2023-03-12 20:27:26 +0000

    contact form

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,29 +1,74 @@
-import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import React, {Component} from "react";
 import './Contact.css'
 
-function Contact() {
-  return (
-    <div className='form'>
+class Contact extends Component {
+    state = {
+        name: "",
+        email: "",
+        text: ""
+    };
+    handleInputChange = event => {
+        // Getting the value and name of the input which triggered the change
+        const { name, value } = event.target;
+    
+        // Updating the input's state
+        this.setState({
+          [name]: value
+        });
+      };
+      handleFormSubmit = event => {
+        // Preventing the default behavior of the form submit (which is to refresh the page)
+        event.preventDefault();
+    
+        // Alert the user their first and last name, clear `this.state.firstName` and `this.state.lastName`, clearing the inputs
+        alert(`Hello ${this.state.name} ${this.state.email} ${this.state.text}`);
+        this.setState({
+          name: "",
+          email: "",
+          text: ""
+        });
+      };
+      render() {
+        return (
+            <div className='form'>
     <Form>
+    <Form.Group className="mb-3" controlId="formBasicName">
+        <Form.Control 
+        value={this.state.name}
+        name="name"
+        onChange={this.handleInputChange}
+        type="text"
+        placeholder="Name" 
+        />
+      </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control 
+        value={this.state.email}
+        name="email"
+        onChange={this.handleInputChange}
+        type="text"
+        placeholder="Email"  />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
+      
       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
         
-        <Form.Control as="textarea" rows={3} />
+        <Form.Control as="textarea" rows={3} 
+        value={this.state.text}
+        name="text"
+        onChange={this.handleInputChange}
+        type="text"
+        placeholder="Comment" 
+        />
       </Form.Group>
-      <Button variant="primary" type="submit">
+      <Button onClick={this.handleFormSubmit} variant="primary" type="submit">
         Submit
       </Button>
     </Form>
     </div>
-  );
+        )
+      }
 }
 
 
